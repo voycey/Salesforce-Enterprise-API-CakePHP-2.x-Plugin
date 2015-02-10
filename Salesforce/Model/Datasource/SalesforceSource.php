698199612f3dc7ce05c55502cee5a83eae52d2a1
Dan Voyce <voycey@users.noreply.github.com>
2015-02-10 14:58:37 +1100

    Update SalesforceSource.php
    Removed copyright as now MIT

--- Salesforce/Model/Datasource/SalesforceSource.php
+++ Salesforce/Model/Datasource/SalesforceSource.php
@@ -3,16 +3,6 @@
 /*  Cakephp 2.x Salesforce Datasource
     Website - http://www.danielvoyce.com
     Author - Daniel Voyce
-
-    Salesforce Datasource is a copyrighted work of authorship. Daniel Voyce retains ownership of the product and any copies of it, regardless of the form in which the copies may exist. This license is not a sale of the original product or any copies.
-    By installing and using this datasource on your server, you agree to the following terms and conditions. Such agreement is either on your own behalf or on behalf of any corporate entity which employs you or which you represent ('Corporate Licensee'). In this Agreement, 'you' includes both the reader and any Corporate Licensee and Daniel Voyce.
-    The Product is licensed only to you. You may not rent, lease, sublicense, sell, assign, pledge, transfer or otherwise dispose of the Product in any form, on
-    a temporary or permanent basis, without the prior written consent of Daniel Voyce.
-    The Product source code may be altered (at your risk)
-    All Product copyright notices within the scripts must remain unchanged (and visible).
-    If any of the terms of this Agreement are violated, Daniel Voyce reserves the right to action against you.
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Product.
-    THE PRODUCT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE PRODUCT OR THE USE OR OTHER DEALINGS IN THE PRODUCT.
 */
 
     App::uses('HttpSocket', 'Network/Http');
